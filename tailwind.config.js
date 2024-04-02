f7e1c440f59deaa3a3ffcf2f475a78401f1a8a5f
Olatunji Peter <polalekan526@gmail.com>
2024-04-02 21:57:16 +0100

    Testing Testing Testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+      // // colors:{
+      // //   'public':'#2d4000',
+      // //   'private':'#8AB863',
+      // //   'private2':'rgba(253, 151, 45, .8)',
+      // //   'white': '#ffffff',
+      // //   'black':'#000000',
+      // //   'gray':'#c7c6c1',
+      // //   'red':'#FF0000',
+      // //   'vintage':'#540202',    
+      // //   'grayLight':'#ececec',
+      // //   'blue':'#0000FF'
+      // // },
+      // fontFamily:{
+      //   roboto:["Roboto", "sans-serif"],
+      //   lora:["Lora", "serif"],
+      //   playfair:['Playfair Display', 'sans-serif']
+  
+      // },
+    extend: {
+    },
   },
   plugins: [],
 }
