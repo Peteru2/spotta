1406fdff4e8e1bba77d1f61d2e1d1151f59b3d41
Olatunji Peter <polalekan526@gmail.com>
2024-04-08 15:37:51 +0100

    Update added

--- src/Views/AllReviews.jsx
+++ src/Views/AllReviews.jsx
@@ -1,20 +1,25 @@
 import navAvatar from "../assets/images/navAvatar.jfif"
 import Navbar from "../GeneralComponents/Navbar";
+import { StarOutlined, LikeOutlined, DislikeOutlined, CommentOutlined } from '@ant-design/icons';
+
 
 const AllReviews = () => {
     return ( 
         <>
         <Navbar />
-        <div>
+        <div className="md:mx-[100px] mx-[50px] mt-[20px]">
             
             <section className="grid grid-cols-3 gap-4 font-roboto ">
-                    <div className="cols-span-3 ">
-                        <div className="flex">
-                            <div className="flex text-xs">
+                    <div className="col-span-2 ">
+                        <div className="flex items-center w-full">
+                            <div className="flex text-xs items-center">
                                     <img className="w-[30px] h-[30px] rounded-full" src={navAvatar} alt="avatar"/>
-                                    <h2>James T.</h2>
-                                    <h2 className="text-gray-200">5 months ago</h2>
+                                    <h2 className="mx-2">James T.</h2>
+                                    <h2 className="text-gray-400">5 months ago</h2>
                             </div>
+                            <h2 className="ml-auto">
+                            <StarOutlined  className="text-yellow-400 " /> 4.0
+                            </h2>
                         </div>
                     </div>
             </section>
