573350274b8a524750968f280967bc1f4ee56a57
Olatunji Peter <polalekan526@gmail.com>
2024-04-09 23:18:44 +0100

    New Update added

--- src/Views/Dashboard.jsx
+++ src/Views/Dashboard.jsx
@@ -3,14 +3,13 @@ import ScrollableDiv from "../DashboardComponents/SrollableDiv";
 const Dashboard = () => {
     return ( 
         <>
-                <div className="flex  md:px-[100px] w-full px-[50px] gap-[320px] items-center font-roboto">
+                <div className="md:flex  md:px-[100px] w-full px-[30px] gap-[320px] items-center font-roboto">
                     <div className="w-[520px]">
                         <h2 className="text-[54px] font-bold ">Find a place you will love to live!</h2>
                         <h2>See through the lenses of people who have
                             lived or visited the neighbourhood you might
                             have in mind.
                         </h2>
-
                         <div className="mt-8">
                             <div  className="border-[1px] py-1 px-2 rounded border-gray-400 w-full border">
                             <input 
@@ -22,8 +21,10 @@ const Dashboard = () => {
                             <button className="bg-blue-500 px-4  my-4 text-white rounded-md py-2 text-sm">SEARCH</button>
                         </div>
                     </div>
-
+                <div>                    
                     <ScrollableDiv />
+                </div>
+
                 </div>
         </>
      );
